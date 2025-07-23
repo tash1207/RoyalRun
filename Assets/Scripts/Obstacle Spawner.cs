84af163f09af805ce0094fe4879e4f8d4c1d222a
Tasha Hankewych <thankewych@gmail.com>
2025-07-23 18:32:23 -0500

    Add more obstacle variants including a barrel, chair, and crate

--- Assets/Scripts/Obstacle Spawner.cs
+++ Assets/Scripts/Obstacle Spawner.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] GameObject[] obstaclePrefabs;
     [SerializeField] float obstacleSpawnTime = 2f;
+    [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth = 3f;
 
     void Start()
     {
@@ -15,8 +17,11 @@ public class ObstacleSpawner : MonoBehaviour
     {
         while (true)
         {
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+            Vector3 spawnPosition = new Vector3(
+                Random.Range(-spawnWidth, spawnWidth), transform.position.y, transform.position.z);
             yield return new WaitForSeconds(obstacleSpawnTime);
-            Instantiate(obstaclePrefab, transform.position, Random.rotation);
+            Instantiate(obstaclePrefab, spawnPosition, Random.rotation, obstacleParent);
         }
     }
 }

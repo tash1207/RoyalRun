73016fbbb0e88986b5aa36bb75c69529db508523
Tasha Hankewych <thankewych@gmail.com>
2025-07-24 14:58:07 -0500

    Start setting up collision handling between player and obstacles and pickups

---
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerCollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+}

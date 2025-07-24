d467c249a141b5cb470b1c87c4c9575cbc330022
Tasha Hankewych <thankewych@gmail.com>
2025-07-24 17:15:18 -0500

    Trigger a stumble animation when the player hits an obstacle

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -2,8 +2,27 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+
+    const string hitTrigger = "Hit";
+
+    float cooldownDuration = 1f;
+    float hitCooldownTimer = 1f;
+
+    void Update()
+    {
+        if (hitCooldownTimer > 0)
+        {
+            hitCooldownTimer -= Time.deltaTime;
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject.name);
+        if (hitCooldownTimer <= 0)
+        {
+            animator.SetTrigger(hitTrigger);
+            hitCooldownTimer = cooldownDuration;
+        }
     }
 }

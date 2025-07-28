ca1e94f5dfc2afc1c4f02185976f0b3c37a27196
Tasha Hankewych <thankewych@gmail.com>
2025-07-28 12:45:46 -0500

    Use inheritance to create different behaviors when an apple or coin is picked up

--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
+public abstract class Pickup : MonoBehaviour
 {
+    [SerializeField] float rotationSpeed = 100f;
+
     const string playerTag = "Player";
+
+    void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
-            Debug.Log(other.gameObject.name);
+            OnPickup();
+            Destroy(gameObject);
         }
     }
+
+    protected abstract void OnPickup();
 }
